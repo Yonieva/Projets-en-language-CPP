0d933c4b2b29ab725c6117f2952c04b6b4ef848e
Yoan Onieva <yonieva@c2r6p2.42perpignan.fr>
2025-01-02 17:32:05 +0100

    ok

---
+++ C05/ex01/Form.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonieva <yonieva@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 15:08:49 by yonieva           #+#    #+#             */
+/*   Updated: 2025/01/02 15:25:32 by yonieva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Form
+{
+    private :
+        const std::string name;
+        bool is_sign;
+        const int min_grade_sign;
+        const int min_grade_execute;
+
+
+
+    public :
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
